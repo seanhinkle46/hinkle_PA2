121b8e109bdb012abd27b4b50a12145393ed7387
Sean Hinkle <seanhinkle46@gmail.com>
2020-01-31 22:28:47 -0500

    Small edits

--- hinkle_p2/src/BMICalculator.java
+++ hinkle_p2/src/BMICalculator.java
@@ -1,11 +1,11 @@
-//Sean Hinkle, COP3330, Dr. Hollander
-//the class BMI calculator takes in height and weight (in preferred
-//system of measurement) and outputs BMI with a display of BMI categories
-
+/*Sean Hinkle, COP3330, Dr. Hollander
+ * the class BMICalculator takes in height and weight (in preferred
+ * system of measurement) and outputs the user's BMI with a display of BMI categories
+*/
 import java.util.Scanner;
 
 public class BMICalculator {
-	private Scanner sc = new Scanner(System.in);
+	private Scanner sc = new Scanner(System.in); //declaring a scanner to use throughout the program
 	private int unitType; //Metric = 1, imperial = 2
 	private double weight;
 	private double height;
@@ -15,7 +15,7 @@ public class BMICalculator {
 	public double getWeight() {
 		return weight;
 	}
-
+	//ensures weight is not set beneath 0, if so, exits program
 	private void setWeight(double weight) {
 		if (weight < 0.0) {
 			System.out.println("Program exiting");
@@ -28,7 +28,7 @@ public class BMICalculator {
 	public double getHeight() {
 		return height;
 	}
-
+	//ensures height is not set beneath 0, if so, exits program
 	private void setHeight(double height) {
 		if (this.height < 0.0) {
 			System.out.println("Program exiting");
@@ -46,24 +46,24 @@ public class BMICalculator {
 		return bmiCategory;
 	}
 
+	//calls methods to obtain unit type, and then calls a method to read in data based on the type
 	public void readUserData() {
 		this.readUnitType();
 		this.readMeasurementData();
 	}
 	
-	private int readUnitType() {
-		//Scanner sc = new Scanner(System.in);
+	//Prompts user to select the measurement system they want to use
+	private void readUnitType() {
 		System.out.print("Enter 1 to use the metric system, 2 to use the imperial system: ");
 		this.unitType = sc.nextInt();
 		while (this.unitType != 1 && this.unitType != 2) {
 			System.out.print("Please enter a valid number: ");
 			this.unitType = sc.nextInt();
 		}
-		//sc.close();
-		return this.unitType;
 		
 	}
 	
+	//Initiates the reading of input based on the measurement system
 	private void readMeasurementData() {
 		if (this.unitType == 1) {
 			this.readMetricData();
@@ -72,6 +72,7 @@ public class BMICalculator {
 		}
 	}
 	
+	//Reads in and stores height and weight in metric units
 	private void readMetricData() {
 		System.out.print("Please enter your weight in kilograms: ");
 		setWeight(sc.nextDouble());
@@ -79,21 +80,15 @@ public class BMICalculator {
 		setHeight(sc.nextDouble());
 	}
 	
+	//Reads in and stores height and weight in metric units
 	private void readImperialData() {
 		System.out.print("Please enter your weight in pounds: ");
 		setWeight(sc.nextDouble());
-		if (this.weight < 0.0) {
-			System.out.println("Program exiting");
-			System.exit(0);
-		}
 		System.out.print("Please enter your height in inches: ");
 		setHeight(sc.nextDouble());
-		if (this.height < 0.0) {
-			System.out.println("Program exiting");
-			System.exit(0);
-		}
 	}
 	
+	//Calculates BMI based on unit of measurement
 	public void calculateBmi() {
 		if (this.unitType == 1) {
 			this.bmi = this.weight/(this.height*this.height);
@@ -104,6 +99,7 @@ public class BMICalculator {
 		this.calculateBmiCategory();
 	}
 	
+	//Assigns the appropriate BMI category 
 	private void calculateBmiCategory() {
 		if (this.bmi <= 18.5) {
 			this.bmiCategory = "Underweight";
@@ -114,10 +110,11 @@ public class BMICalculator {
 		else if (this.bmi < 30.0) {
 			this.bmiCategory = "Overweight";
 		} else {
-			this.bmiCategory = "Obesity";
+			this.bmiCategory = "Obese";
 		}
 	}
 	
+	//Displays user's BMI and the BMI classification chart
 	public void displayBmi() {
 		System.out.printf("%nYour BMI is %.2f, you are classified as %s%n",
 					this.getBmi(), this.getBmiCategory());
